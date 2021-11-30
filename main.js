0e2ef8bbce65327ce6fb26014aa56d42a0e7bfd3
project84 <phil.m.burn@gmail.com>
2021-11-30 21:55:32 +0000

    Main file runs specified file and logs result

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Test message');
+import { execSync } from 'child_process';
+
+const args = process.argv.slice(2);
+const day = args.length ? args[0] : '01';
+
+console.log('Executing solution for day ' + day);
+
+// Execute specified file and log result
+const output = execSync(`npx babel-node days/${day}.js`, { encoding: 'utf-8' });
+console.log('Answer is:\n' + output);
