1ab0fa4593770acec8822369df1a1e230f02e557
project84 <36703733+project84@users.noreply.github.com>
2021-12-01 13:39:52 +0000

    Day to run is retrieved from CLI

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+import readlineSync from 'readline-sync';
 import { execSync } from 'child_process';
 
-const args = process.argv.slice(2);
-const day = args.length ? args[0] : '01';
+let day = readlineSync.question('Which day would you like to run? ');
+day = String(day).padStart(2, '0');
 
-console.log('Executing solution for day ' + day);
+console.log('Running solution for day ' + day);
 
 // Execute specified file and log result
 const output = execSync(`npx babel-node days/${day}.js`, { encoding: 'utf-8' });
-console.log('Answer is:\n' + output);
+console.log('The answer is:\n' + output);
