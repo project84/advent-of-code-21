670807014ea3143410d299fcc78d77050de8a6da
Philip Rozycki-Burn <36703733+project84@users.noreply.github.com>
2021-12-01 22:07:19 +0000

    Added check for current month being December

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import readlineSync from 'readline-sync';
-import { access, constants } from 'fs';
+import { access } from 'fs';
 
 const currentDate = new Date();
 let year = currentDate.getFullYear();
 let day = currentDate.getDate();
 
-if (readlineSync.question(`Do you wish to run the solution for day ${day} of ${year}? (Y/N) `).toUpperCase() != 'Y') {
+if (
+  currentDate.getMonth != 12 || 
+  readlineSync.question(`Do you wish to run the solution for day ${day} of ${year}? (Y/N) `).toUpperCase() != 'Y'
+) {
   year = readlineSync.question('Which year would you like to run? ');
   day = readlineSync.question('Which day would you like to run? ');
 }
