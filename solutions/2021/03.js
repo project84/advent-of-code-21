76daeb8a56f60a0408ede2bf0e1a0dbcaa5438f9
Philip Rozycki-Burn <36703733+project84@users.noreply.github.com>
2021-12-03 08:50:45 +0000

    Step 1 solution

---
+++ solutions/2021/03.js
@@ -0,0 +1,27 @@
+export default function(inputFile) {
+	const reportItemLength = inputFile[0].length;
+
+	let condensedReport = inputFile.reduce((digitSum, reportItem) => {
+		for (let i = 0; i < reportItemLength; i++) {
+			digitSum[i] = (digitSum[i] || 0) + parseInt(reportItem[i]);
+		}
+
+		return digitSum;
+	}, {})
+
+	let gammaRate = '';
+	let epsilonRate = '';
+
+	for (let i = 0; i < reportItemLength; i++) {
+		const mostCommonBit = condensedReport[i] >= inputFile.length / 2 ? '1' : '0';
+
+		gammaRate += mostCommonBit;
+		epsilonRate += parseInt(mostCommonBit) ? '0' : '1';
+	}
+
+	return {
+		step1: parseInt(gammaRate, 2) * parseInt(epsilonRate, 2),
+		step2: null
+	}
+
+}
