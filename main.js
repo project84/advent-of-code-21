8ad56e81f4b6a5f88ac6dbf3134f28c5fe0f3be6
Philip Rozycki-Burn <36703733+project84@users.noreply.github.com>
2021-12-07 08:50:11 +0000

    Improved multi-day runner

--- main.js
+++ main.js
@@ -1,47 +1,54 @@
 import readlineSync from 'readline-sync';
-import * as file from './utils/general/file-tools';
+
+import { getSolutionInfo, retrieveTextFile } from './utils/general/file-tools';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
 const argv = yargs(hideBin(process.argv)).argv
 
-let year;
-let day;
+let datesToRun = [];
 
+// Set dates to run based on CLI inputs or queries
 if (argv.today) {
-	// Get current date and set day / year
-	const currentDate = new Date();
-	year = currentDate.getFullYear();
-	day = currentDate.getDate();
-} else if (argv.year && argv.day) {
-	// Read desired date from command line if specified and today NOT specified
-	year = argv.year;
-	day = argv.day
-} else {
-	// Allows the user to select the date to run
-	year = readlineSync.question('Which year would you like to run? ');
-	day = readlineSync.question('Which day would you like to run? ');
+	datesToRun.push(getSolutionInfo());
 }
 
-// Determine file paths for solution and input file, and checks if they exist
-const dateString = `${year}/${String(day).padStart(2, '0')}`;
-const solutionFilePath = `solutions/${dateString}.js`;
-const inputFilePath = `input-files/${dateString}/${argv.example ? 'example' : 'actual'}.txt`;
-
-if (file.exists(solutionFilePath) && file.exists(inputFilePath)) {
-
-	// Retrieve input file content for specified day
-	const inputFile = file.retrieveTextFile(inputFilePath, true);
-
-	console.log(`Running solution for day ${day} of ${year}`);
-	const startTime = new Date();
-
-	// Run solution for specified day and log result
-	const solution = require('./' + solutionFilePath).default(inputFile);
-	console.log(`\nStep 1: ${solution.step1}\nStep 2: ${solution.step2}\n`);
-	console.log((new Date() - startTime) + ' ms');
-
-} else {
-	// Return error if a file is unavailable and exit
-	console.log('Solution or input file not found, check and try again');
+if (argv.year && argv.day) {
+	datesToRun.push(getSolutionInfo(argv.year, argv.day));
 }
+
+if (!argv.today && !(argv.year && argv.day)) {
+	let year = readlineSync.question('Which year would you like to run? ');
+	let day = readlineSync.question('Which day would you like to run? ');
+
+	datesToRun.push(getSolutionInfo(year, day));
+}
+
+// Filter to only dates with solution and input files available
+datesToRun = datesToRun.filter(date => {
+	return date.solution.exists && date.example.exists && date.actual.exists;
+});
+
+if (!datesToRun.length) {
+	console.log('No solutions available to run, please check and try again');
+}
+
+datesToRun.forEach(date => {
+
+	['example', 'actual'].forEach(type => {
+		
+		// Retrieve input file
+		const inputFile = retrieveTextFile(date[type].path, true);
+		const startTime = new Date();
+
+		// Run solution for specified day and log result
+		const solution = require('./' + date.solution.path).default(inputFile);
+
+		const endTime = new Date();
+
+		console.log(`${date.fileString} (${type}) - ${(endTime - startTime)} ms`);
+		console.log(`Step 1: ${solution.step1}\nStep 2: ${solution.step2}\n`);
+
+	});
+
+});
--- utils/general/date-tools.js
+++ utils/general/date-tools.js
@@ -1,12 +1,16 @@
 export function getParsedDate(dateString) {
+
+	// Create date object from input value
 	const rawDate = dateString ? new Date(dateString) : new Date();
 	
+	// Deserialise date into constituent parts
 	let parsedDate = {
 		day: rawDate.getDate(),
 		month: rawDate.getMonth(),
 		year: rawDate.getFullYear()
 	}
 
+	// Retrieve and set file string for date
 	parsedDate.fileString = getDateFileString(parsedDate.year, parsedDate.day);
 
 	return parsedDate;
