ff7aa550b657833a5c0234ca79285a555ad83637
Philip Rozycki-Burn <36703733+project84@users.noreply.github.com>
2021-12-11 08:24:02 +0000

    Day 10 solution

---
+++ solutions/2021/10.js
@@ -0,0 +1,61 @@
+import { checkIllegalChunks  } from '../../utils/2021/navigation';
+import { sum as arraySum } from '../../utils/general/array-tools';
+
+const charScores = {
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
+	'(': 1,
+	'[': 2,
+	'{': 3,
+	'<': 4
+}
+
+export default function(inputFile) {
+
+	/* Example */
+	// Step 1: 26397
+	// Step 2: null
+	
+	/* Actual */
+	// Step 1: 399153
+	// Step 2: null
+
+	// Check navigation readings for illegal lines, and reduce all others to incomplete chunks
+    let navigationOutput = inputFile
+		.map(x => checkIllegalChunks({ value: x }));
+
+	// Determine score for the illegal characters found
+	let illegalCharScore = arraySum(navigationOutput
+		.map(y => {
+			// Illegal character score is mapped from static data
+			return y.illegalCharacter ?
+				charScores[y.illegalCharacter] :
+				0;
+		}));
+
+	// Determine autocomplete scores for incomplete lines and sort in ascending order to allow retrieval of the middle score
+	let autoCompleteScore = navigationOutput
+		.filter(x => !x.illegalCharacter)
+		.map(y => {
+			
+			const reading = y.value;
+			let score = 0;
+
+			// Autocomplete score multiplies by 5, then adds mapped score from static
+			for (let i = reading.length - 1; i >= 0; i--) {
+				score = ( score * 5 ) + charScores[reading[i]];
+			}
+
+			return score;
+		})
+		.sort((a, b) => a - b);
+
+	
+    return {
+		step1: illegalCharScore,
+		step2: autoCompleteScore[Math.floor(autoCompleteScore.length / 2)]
+	}
+	
+}
