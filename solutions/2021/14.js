a84dd5b1afdcf9604d378b41dec9d332995172ca
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2021-12-14 09:04:50 +0000

    Added answer comments to day 14 solution

--- solutions/2021/14.js
+++ solutions/2021/14.js
@@ -2,6 +2,14 @@ import { Polymer } from '../../utils/2021/polymer';
 
 export default function (inputFile) {
 
+    /* Example */
+    // Step 1: 1588
+    // Step 2: 2188189693529
+
+    /* Actual */
+    // Step 1: 2233
+    // Step 2: 2884513602164
+
     const polymer = new Polymer(inputFile);
     let countAt10;
 
