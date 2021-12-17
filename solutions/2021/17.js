2bdcf4ea4693b283a37c8a0a504903b7586421dc
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2021-12-17 10:42:16 +0000

    Updated comment

--- solutions/2021/17.js
+++ solutions/2021/17.js
@@ -13,7 +13,7 @@ export default function(inputFile) {
     let probeLauncher = new ProbeLauncher(inputFile[0]);
 
     // Loop through all values that could hit the target to check if they do
-    // x velocity cannot be less than 0, or greater than the max x position in the target area
+    // x velocity cannot be less than the minimum x velocity, or greater than the max x position in the target area
     // y velocity cannot be less than the minimum y position in the target area, 
     // or greater than the max Y velocity
     let validVelocityCount = 0;
