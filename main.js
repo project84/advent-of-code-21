aad226e6c4ccaf020660d6a812cc435685650091
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-01-25 12:46:37 +0000

    Converted main flow to use case statement instead of successive ifs

--- main.js
+++ main.js
@@ -9,48 +9,45 @@ try {
 	let requestedDates = getRequestedDates();
 	const solutionTypes = getSolutionTypes();
 
-	if (argv.solution) {
+	let exType = argv.exType;
 
-		// Filter requested dates to only those where the solution exists, exiting if there are none
-		let solutionsToRun = requestedDates.filter(date => date.solution.exists && date.exampleExists && date.actualExists);
-		if (!solutionsToRun.length) {
-			throw new Error('No solutions available to run');
-		}
+	switch (exType) {
 
-		runSolutions(solutionsToRun, solutionTypes);
+		case 'solution':
+			// Filter requested dates to only those where the solution exists, exiting if there are none
+			let solutionsToRun = requestedDates.filter(date => date.solution.exists && date.exampleExists && date.actualExists);
+			if (!solutionsToRun.length) {
+				throw new Error('No solutions available to run');
+			}
 
-	}
+			runSolutions(solutionsToRun, solutionTypes);
+			break;
 
-	if (argv.verify) {
+		case 'verify':
+			// Validate input parameters, then verify solutions
+			let verifyParams = validateAnswerVerificationParams(requestedDates, solutionTypes);
+			verifyAnswers(requestedDates, solutionTypes, verifyParams);
+			break;
 
-		// Validate input parameters, then verify solutions
-		let params = validateAnswerVerificationParams(requestedDates, solutionTypes);
-		verifyAnswers(requestedDates, solutionTypes, params);
+		case 'known':
+			// Print known answers for requested dates
+			printKnownAnswers(requestedDates, solutionTypes);
+			break;
 
-	}
+		case 'initialise':
+			// Validate input parameters for solution initialisation
+			let initialiseParams = validateInitialisationParams(requestedDates);
 
-	if (argv.known) {
+			// Create empty solution file for the requested date, and empty input file folder
+			copyFileSync('fixtures/general/solution-template.js', initialiseParams.path);
+			mkdirSync('input-files/' + initialiseParams.date);
 
-		// Print known answers for requested dates
-		printKnownAnswers(requestedDates, solutionTypes);
-
-	}
-
-	if (argv.initialise) {
-
-		// Validate input parameters for solution initialisation
-		let params = validateInitialisationParams(requestedDates);
-
-		// Create empty solution file for the requested date, and empty input file folder
-		copyFileSync('fixtures/general/solution-template.js', params.path);
-		mkdirSync('input-files/' + params.date);
-
-		// Record verified answer if supplied
-		if (params['1']) {
-			verifyAnswers(requestedDates, [ 'example' ], params);
-		}
-		
-		console.log(params.date + ' solution initialised - input files must be added to the repo manually.')
+			// Record verified answer if supplied
+			if (initialiseParams['1']) {
+				verifyAnswers(requestedDates, [ 'example' ], initialiseParams);
+			}
+			
+			console.log(initialiseParams.date + ' solution initialised - input files must be added to the repo manually.');
 
 	}
 
