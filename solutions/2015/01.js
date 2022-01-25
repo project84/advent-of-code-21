d741f614edb3c557c7a04f1d1ea186f7278aa121
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-01-25 14:28:32 +0000

    Solution for 2015 day 1

--- solutions/2015/01.js
+++ solutions/2015/01.js
@@ -1,9 +1,23 @@
 export default function (inputFile) {
 
+	// Create array of individual instructions and count up / down instructions
+	let directions = inputFile[0].split('');
+	let countUp = directions.filter(direction => direction === '(').length;
+	let countDown = directions.filter(direction => direction === ')').length;
 
+	let currentPosition = 0;
+	let directionsProcessed = 0;
+
+	while (currentPosition >= 0 && directions.length) {
+
+		// Parse instructions until a basement floor is reached
+		currentPosition += directions.shift() === '(' ? 1 : -1;
+		directionsProcessed++;
+
+	}
 
 	return {
-		1: null,
-		2: null
+		1: countUp - countDown,
+		2: directionsProcessed
 	}
 }
