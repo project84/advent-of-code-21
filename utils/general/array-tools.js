5acaf7c8f10bab0626234669e754840064a0a358
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-02-02 09:37:34 +0000

    Array sum and multiply functions are now safe when a 0 length array is passed

--- utils/general/array-tools.js
+++ utils/general/array-tools.js
@@ -1,4 +1,10 @@
 export function sum(inputArray) {
+
+	// If there are no items in the array, sum is 0 (blocks failure on attempting to reduce 0 item array)
+	if (!inputArray.length) {
+		return 0;
+	}
+
 	// Adds all items in a provided array of integers
 	return inputArray.reduce((prev, next) => {
 		return parseInt(prev) + parseInt(next);
@@ -6,6 +12,12 @@ export function sum(inputArray) {
 }
 
 export function multiply(inputArray) {
+
+	// If there are no items in the array, product is 0 (blocks failure on attempting to reduce 0 item array)
+	if (!inputArray.length) {
+		return 0;
+	}
+
 	// Multiply all items in a provided array of integers
 	return inputArray.reduce((prev, next) => {
 		return parseInt(prev) * parseInt(next);
