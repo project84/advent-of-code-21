8bef2f1a0238808a161c421b207c70b3c8561372
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-01 21:04:53 +0000

    Part 2 solution with priority queue

--- solutions/2022/01.js
+++ solutions/2022/01.js
@@ -1,21 +1,29 @@
+import { sum } from "../../utils/general/array-tools";
+import { PriorityQueue } from "../../utils/general/priority-queue";
+
 export default function (inputFile) {
 
-	const { elves } = inputFile.reduce((calorieStats, snack) => {
-		if (!snack.length) {
+	const { queue } = inputFile.reduce((calorieStats, snack, i) => {
+		const calories = calorieStats.current + +snack;
+
+		if (!snack.length || i === inputFile.length - 1) {
+			calorieStats.queue.enqueue(calories, calories)
 			return {
-				current: 0,
-				elves: [ ...calorieStats.elves, calorieStats.current ]
+				...calorieStats,
+				current: 0
 			}
 		}
 
 		return {
 			...calorieStats,
-			current: calorieStats.current + +snack
+			current: calories
 		};
-	}, { current: 0, elves: [] })
+	}, { current: 0, queue: new PriorityQueue() });
+
+	const elves = queue.items.map((qItem) => qItem.element);
 
 	return {
-		1: Math.max(...elves),
-		2: null
+		1: elves[elves.length - 1],
+		2: sum(elves.slice(elves.length - 3))
 	}
 }
