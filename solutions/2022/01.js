ba2583c3d18ed014c6069bd59477f9380d26b335
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-01 21:07:54 +0000

    Comments

--- solutions/2022/01.js
+++ solutions/2022/01.js
@@ -3,10 +3,13 @@ import { PriorityQueue } from "../../utils/general/priority-queue";
 
 export default function (inputFile) {
 
-	const { queue } = inputFile.reduce((calorieStats, snack, i) => {
+	// Parse input file to list of calories carried by each elf
+	const elves = inputFile.reduce((calorieStats, snack, i) => {
+		// Add next snack calories to current elf calories
 		const calories = calorieStats.current + +snack;
 
 		if (!snack.length || i === inputFile.length - 1) {
+			// Queue elf calories when all snacks have been counted for the elf
 			calorieStats.queue.enqueue(calories, calories)
 			return {
 				...calorieStats,
@@ -18,9 +21,10 @@ export default function (inputFile) {
 			...calorieStats,
 			current: calories
 		};
-	}, { current: 0, queue: new PriorityQueue() });
-
-	const elves = queue.items.map((qItem) => qItem.element);
+	}, { current: 0, queue: new PriorityQueue() })
+		.queue
+		.items
+		.map((qItem) => qItem.element);
 
 	return {
 		1: elves[elves.length - 1],
