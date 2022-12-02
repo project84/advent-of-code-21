d9d80e70206b1eef576e6c09224d9227d860d6a7
Philip <philip.rozycki-burn@techspert.io>
2022-12-02 08:34:39 +0000

    2022/02 - part 1 solution

--- solutions/2022/02.js
+++ solutions/2022/02.js
@@ -1,9 +1,22 @@
+import rockPaperSciccors from '../../fixtures/2022/rock-paper-scissors.json'
+
 export default function (inputFile) {
 
+	const rounds = inputFile.map((round) => {
+		const [opponent, player] = round.split(' ');
+		return { opponent, player }
+	});
 
+	const strategyScore = rounds.reduce((score, round) => {
+		const shapeScore = rockPaperSciccors.shapes.find((shape) => shape.player === round.player).score;
+		const outcome = rockPaperSciccors.outcomes.find((outcome) => outcome.opponent === round.opponent && outcome.player === round.player);
+		const outcomeScore = rockPaperSciccors.scores.find((score) => score.isOpponentWin === outcome.isOpponentWin &&  score.isPlayerWin === outcome.isPlayerWin).score;
+
+		return score + shapeScore + outcomeScore;
+	}, 0)
 
 	return {
-		1: null,
+		1: strategyScore,
 		2: null
 	}
 }
