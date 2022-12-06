3d18abead839a1d287d6b4649ba86278c8004fc6
Philip <philip.rozycki-burn@techspert.io>
2022-12-06 08:19:27 +0000

    2022/06 - part 1 solution and verified answers

--- solutions/2022/06.js
+++ solutions/2022/06.js
@@ -1,9 +1,20 @@
 export default function (inputFile) {
 
+	const dataStream = inputFile[0].split('');
+	
+	let markerPosition;
+	let i = 0;
 
+	while (!markerPosition) {
+		if ([...new Set(dataStream.slice(i, i + 4))].length === 4) {
+			markerPosition = i + 4;
+		}
+
+		i++;
+	}
 
 	return {
-		1: null,
+		1: markerPosition,
 		2: null
 	}
 }
