fb07b1cad16819ec461f3856e05cdc85c33fc8e6
Philip <philip.rozycki-burn@techspert.io>
2022-12-08 08:21:31 +0000

    2022/07 - improvements

--- solutions/2022/07.js
+++ solutions/2022/07.js
@@ -2,32 +2,20 @@ import { sum } from "../../utils/general/array-tools";
 
 export default function (inputFile) {
 
-	const { filesystem } = inputFile.reduce(({ current, filesystem, i }, command) => {
+	const { filesystem } = inputFile.reduce(({ current, filesystem }, command) => {
 
 		const [ dir ] = command.match(/(?<=\$ cd )[^\.]+/g) || [];
 
 		if (dir) {
 			current = dir === '/' ? 'root' : `${current}/${dir}`;
-			filesystem[current] = {
-				children: [],
-				files: []
-			};
-		}
-
-		const [ child ] = command.match(/(?<=dir ).+/) || [];
-
-		if (child) {
-			filesystem[current].children = [
-				...filesystem[current].children,
-				child
-			]
+			filesystem[current] = [];
 		}
 
 		const [ file ] = command.match(/\d+?(?= .+)/) || []
 
 		if (file) {
-			filesystem[current].files = [
-				...filesystem[current].files,
+			filesystem[current] = [
+				...filesystem[current],
 				+file
 			]
 		}
@@ -41,7 +29,7 @@ export default function (inputFile) {
 	}, { filesystem: {} });
 
 	const dirList = Object.keys(filesystem);
-	const dirSizes = dirList.map((dir) => sum(dirList.filter((path) => path.startsWith(dir)).flatMap((path) => filesystem[path].files)));
+	const dirSizes = dirList.map((dir) => sum(dirList.filter((path) => path.startsWith(dir)).flatMap((path) => filesystem[path])));
 
 	const target = 30000000 - (70000000 - Math.max(...dirSizes));
 
