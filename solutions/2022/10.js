578a1633658a2a3e4d5e9abc4608f165e3eab178
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-10 13:18:00 +0000

    2022/10 - part 1 solution

--- solutions/2022/10.js
+++ solutions/2022/10.js
@@ -1,9 +1,25 @@
+import { sum } from '../../utils/general/array-tools';
+
 export default function (inputFile) {
 
+	const { cycles } = inputFile.reduce(({ cycles, currentCycle, currentValue }, command) => {
 
+		currentCycle++;
+		cycles[currentCycle] = currentValue;
+		const [, add] = command.split(' ');
+
+		if (isFinite(add)) {
+			currentCycle++;
+			cycles[currentCycle] = currentValue;
+			currentValue += +add;
+		}
+
+		return { cycles, currentCycle, currentValue }
+
+	}, { cycles: {}, currentCycle: 0, currentValue: 1 });
 
 	return {
-		1: null,
+		1: sum(Array.from({ length: 6 }, (_, i) => 20 + i * 40).map((cycle) => cycles[cycle] * cycle)),
 		2: null
 	}
 }
