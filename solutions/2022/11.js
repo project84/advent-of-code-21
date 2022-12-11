941ea415d58b655156cf145776493318f6a79284
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-11 21:03:48 +0000

    2022/11 - refactor and timing

--- solutions/2022/11.js
+++ solutions/2022/11.js
@@ -8,7 +8,23 @@ export default function (inputFile) {
 		return new Monkey(inputFile.slice(descriptionIndex + 1, descriptionIndex + 6))
 	});
 
-	for (let i = 0; i < 20; i++) {
+	processRounds(20, monkeys);
+
+	const monkeyBusiness = calculateMonkeyBusiness(monkeys);
+
+	const reducer = multiply(monkeys.map((monkey) => monkey.test));
+	monkeys.forEach((monkey) => monkey.reset(false, reducer));
+
+	processRounds(10000, monkeys);
+
+	return {
+		1: monkeyBusiness,
+		2: calculateMonkeyBusiness(monkeys)
+	}
+}
+
+const processRounds = (numberOfRounds, monkeys) => {
+	for (let i = 0; i < numberOfRounds; i++) {
 		monkeys.forEach((monkey) => {
 			while (monkey.items.length) {
 				const { item, throwTo } = monkey.throw();
@@ -16,25 +32,7 @@ export default function (inputFile) {
 			}
 		});
 	}
-
-	const monkeys2 = Array.from({ length: inputFile.filter((command) => /Monkey \d:/.test(command)).length }, (_, i) => {
-		const descriptionIndex = inputFile.indexOf(`Monkey ${i}:`);
-		return new Monkey(inputFile.slice(descriptionIndex + 1, descriptionIndex + 6))
-	});
-
-	const divisor = multiply(monkeys2.map((monkey) => monkey.test));
-
-	for (let i = 0; i < 10000; i++) {
-		monkeys2.forEach((monkey) => {
-			while (monkey.items.length) {
-				const { item, throwTo } = monkey.throw(divisor);
-				monkeys2[throwTo].catch(item);
-			}
-		});
-	}
-
-	return {
-		1: multiply(monkeys.map((monkey) => monkey.thrown).sort((a, b) => b - a).slice(0, 2)),
-		2: multiply(monkeys2.map((monkey) => monkey.thrown).sort((a, b) => b - a).slice(0, 2))
-	}
 }
+
+const calculateMonkeyBusiness = (monkeys) => 
+	multiply(monkeys.map((monkey) => monkey.inspected).sort((a, b) => b - a).slice(0, 2))
--- utils/2022/keep-away-monkey.js
+++ utils/2022/keep-away-monkey.js
@@ -1,27 +1,43 @@
 export class Monkey {
-    constructor(description) {
+    constructor(description, worryReduction = true) {
         const [items, operation, test, pass, fail] = description;
         
-        this.items = items.match(/\d+?\b/g).map((item) => +item);
+        this.startItems = items.match(/\d+?\b/g).map((item) => +item);
 
         const operationParts = operation.split(' = ')[1].split(' ');
-        this.operation = (x) => {
-            const value = operationParts[2] === 'old' ? x : +operationParts[2];
-            return operationParts[1] === '+' ? x + value : x * value
+
+        this.operationValue = +operationParts[2];
+        if (operationParts[1] === '+') {
+            this.operationValue ? 
+                this.operation = (x) => x + this.operationValue : 
+                this.operation = (x) => x + x;
+        } else {
+            this.operationValue ? 
+                this.operation = (x) => x * this.operationValue : 
+                this.operation = (x) => x * x;
         }
 
         this.test = +test.match(/\d+?\b/)[0];
         this.pass = +pass.match(/\d+?\b/)[0];
         this.fail = +fail.match(/\d+?\b/)[0];
 
-        this.thrown = 0;
+        this.reset(worryReduction);
     }
 
-    throw(reducer) {
-        this.thrown++;
+    reset(worryReduction, reducer) {
+        this.worryReduction = worryReduction;
+        this.reducer = reducer;
+        this.items = [...this.startItems];
+        this.inspected = 0;
+    }
+
+    throw() {
+        this.inspected++;
 
         const item = this.items.shift();
-        const worryValue = reducer ? this.operation(item) % reducer : Math.floor(this.operation(item) / 3);
+        const worryValue = this.worryReduction ? 
+            Math.floor(this.operation(item) / 3) : 
+            this.operation(item) % this.reducer ;
 
         return {
             item: worryValue,
