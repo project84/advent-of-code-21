a8038f1397da037b1100fe41910219e45cb9afdf
Philip <philip.rozycki-burn@techspert.io>
2022-12-12 14:09:25 +0000

    2022/12 - comments

--- solutions/2022/12.js
+++ solutions/2022/12.js
@@ -5,6 +5,7 @@ export default function (inputFile) {
 
 	const heightMap = new PathFinder(inputFile, false);
 
+	// Find initially defined start and end positions and remap to the expected height value
 	const initalStartPos = heightMap.map.findIndex((position) => position.value === 'S');
 	const endPos = heightMap.map.findIndex((position) => position.value === 'E');
 
@@ -12,6 +13,8 @@ export default function (inputFile) {
 	heightMap.map[endPos].value = 'z';
 
 	return heightMap.map.filter((position) => position.value === 'a').map((position) => position.index).reduce((solution, position) => {
+
+		// Find the shortest possible path to the end position from each possible starting position
 		heightMap.findShortestPath(position, 
 			(nextNode, neighbour) => lower.indexOf(neighbour) <= lower.indexOf(nextNode) + 1, 
 			(nextNode) => nextNode.distance + 1
@@ -19,10 +22,12 @@ export default function (inputFile) {
 
 		const pathLength = heightMap.map[endPos].distance;
 
+		// Solution for part 1 is the defined start poistion
 		if (position === initalStartPos) {
 			solution['1'] = pathLength;
 		}
 
+		// Solution for part 2 is the shortest possible path
 		if (pathLength < solution['2']) {
 			solution['2'] = pathLength;
 		}
@@ -32,5 +37,5 @@ export default function (inputFile) {
 		return solution;
 
 	}, { '1': 0, '2': Infinity });
-	
+
 }
