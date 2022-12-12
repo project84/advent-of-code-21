950f3d01ded587a391bc04354ec1cc5e93f6f629
Philip <philip.rozycki-burn@techspert.io>
2022-12-12 12:06:22 +0000

    2022/12 - updated path finder and part 1 solution

--- solutions/2022/12.js
+++ solutions/2022/12.js
@@ -1,7 +1,23 @@
+import { PathFinder } from '../../utils/general/path-finder'
+import { lower } from "../../fixtures/general/language/constants";
+
 export default function (inputFile) {
 
+	const heightMap = new PathFinder(inputFile, false);
+
+	const startPos = heightMap.map.findIndex((position) => position.value === 'S');
+	const endPos = heightMap.map.findIndex((position) => position.value === 'E');
+
+	heightMap.map[startPos].value = 'a';
+	heightMap.map[endPos].value = 'z';
+
+	heightMap.findShortestPath(startPos, 
+		(nextNode, neighbour) => lower.indexOf(neighbour) <= lower.indexOf(nextNode) + 1, 
+		(nextNode) => nextNode.distance + 1
+	);
+
 	return {
-		1: null,
+		1: heightMap.map[endPos].distance,
 		2: null
 	}
 }
