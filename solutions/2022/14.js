ea1c8cd97d23d6ef4fd1b6a03265104e5c15a0cf
Philip <philip.rozycki-burn@techspert.io>
2022-12-14 16:31:54 +0000

    comments

--- solutions/2022/14.js
+++ solutions/2022/14.js
@@ -1,11 +1,14 @@
 import { SandParticle } from '../../utils/2022/sand-particles';
 
 export default function (inputFile) {
+  // Parse input file to dictionary of rocks in the cave and determine height / width of the cave
   const { cave, minX, maxX, maxY } = inputFile.reduce(
     ({ cave, minX, maxX, maxY }, path) => {
       const nodes = path.split(' -> ');
       const { rocks, rockMinX, rockMaxX, rockMaxY } = nodes.reduce(
         ({ rocks, lastRock, rockMinX, rockMaxX, rockMaxY }, node) => {
+          // Get coordinates for the next node in the rock path
+          // Update cave width / height if this is now known to have changed
           const [x, y] = node.split(',').map((coordinate) => +coordinate);
 
           rockMinX = x < rockMinX ? x : rockMinX;
@@ -13,11 +16,15 @@ export default function (inputFile) {
           rockMaxY = y > rockMaxY ? y : rockMaxY;
 
           if (!lastRock) {
+            // Add the first rock in the path to the list of rocks
             rocks[`${x}|${y}`] = {
               isRock: true,
             };
             lastRock = { x, y };
           } else {
+            // Otherwise determine how far away the node is from the previous
+            // and add rocks to the dictionary for new points up to and including the
+            // next node
             const xDif = x - lastRock.x;
             const yDif = y - lastRock.y;
 
@@ -36,6 +43,7 @@ export default function (inputFile) {
         { rocks: {}, rockMinX: Infinity, rockMaxX: 0, rockMaxY: 0 }
       );
 
+      // Add all new rocks to the cave
       cave = { ...cave, ...rocks };
       minX = rockMinX < minX ? rockMinX : minX;
       maxX = rockMaxX > maxX ? rockMaxX : maxX;
@@ -48,9 +56,11 @@ export default function (inputFile) {
 
   let i = 0;
   let part1 = 0;
-  let complete = false;
+  let part2 = 0;
 
-  while (!complete) {
+  // Drop sand particles until one blocks the cave entrance
+  while (!part2) {
+    // Create and drop the sand particle
     const sand = new SandParticle(cave, maxX, maxY);
     sand.fall();
 
@@ -60,11 +70,13 @@ export default function (inputFile) {
         sand.position.x <= minX ||
         sand.position.y >= maxY)
     ) {
+      // Part 1 is the point at which the sand particles drops outside the
+      // inititally defined grid and falls to infinity
       part1 = i;
     }
 
     if (sand.position.x === 500 && sand.position.y === 0) {
-      complete = true;
+      part2 = i + 1;
     }
 
     i++;
@@ -72,6 +84,6 @@ export default function (inputFile) {
 
   return {
     1: part1,
-    2: i,
+    2: part2,
   };
 }
--- utils/2022/sand-particles.js
+++ utils/2022/sand-particles.js
@@ -1,13 +1,16 @@
 export class SandParticle {
   constructor(cave, width, height) {
+    // Set start position and ingest current cave
     this.position = { x: 500, y: 0 };
+
     this.cave = cave;
     this.caveWidth = width;
     this.caveHeight = height;
   }
 
   fall() {
-    while (this.getNextPosition() && this.position.y < this.caveHeight + 1) {
+    // Sand particles fall until they cannot fall any further, the position is updated when the particle has settled
+    while (this.getNextPosition()) {
       this.position = this.getNextPosition();
     }
 
@@ -15,18 +18,29 @@ export class SandParticle {
   }
 
   getNextPosition() {
+    // Determines the next possible position for the sand particle when falling
+
+    // First calculate which y position the particle falls to, if the particle
+    // has reached the floor, it cannot move further
     const nextY = this.position.y + 1;
-
-    if (!this.cave[`${this.position.x}|${nextY}`]) {
-      return { x: this.position.x, y: nextY };
+    if (nextY === this.caveHeight + 2) {
+      return;
     }
 
-    if (!this.cave[`${this.position.x - 1}|${nextY}`]) {
-      return { x: this.position.x - 1, y: nextY };
-    }
+    return [0, -1, 1].reduce((newPosition, move) => {
+      // If the particle hasn't reached the floor check whether the position immediately below
+      // is occupied, then again for the positions one to the left and right. If all positions
+      // are occupied, the particle can't fall further
 
-    if (!this.cave[`${this.position.x + 1}|${nextY}`]) {
-      return { x: this.position.x + 1, y: nextY };
-    }
+      // Stop processing if we'd already found the next position
+      if (newPosition) {
+        return newPosition;
+      }
+
+      const nextX = this.position.x + move;
+      if (!this.cave[`${nextX}|${nextY}`]) {
+        return { x: nextX, y: nextY };
+      }
+    }, null);
   }
 }
