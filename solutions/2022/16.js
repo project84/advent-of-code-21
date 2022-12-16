5172d344978e83fe2ac14b36c72dd56b5b713216
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-16 20:31:41 +0000

    not very optimal part 1

--- solutions/2022/16.js
+++ solutions/2022/16.js
@@ -1,110 +1,85 @@
 import { PriorityQueue } from '../../utils/general/priority-queue';
 
-export default function(inputFile) {
-	const { valves, closedValves } = inputFile.reduce(
-		({ valves, closedValves }, reading) => {
-			const [valve, ...connections] = reading.match(/[A-Z]{2}/g);
-			const [flowRate] = reading
-				.match(/(?<=\=)\d+/)
-				.map((flowRate) => +flowRate);
+export default function (inputFile) {
+  const { valves, closedValves } = inputFile.reduce(
+    ({ valves, closedValves }, reading) => {
+      const [valve, ...connections] = reading.match(/[A-Z]{2}/g);
+      const [flowRate] = reading
+        .match(/(?<=\=)\d+/)
+        .map((flowRate) => +flowRate);
 
-			valves[valve] = {
-				connections,
-				flowRate
-			};
-			if (flowRate) {
-				closedValves.push(valve);
-			}
+      valves[valve] = {
+        connections,
+        flowRate,
+      };
+      if (flowRate) {
+        closedValves.push(valve);
+      }
 
-			return { valves, closedValves };
-		},
-		{ valves: {}, closedValves: [] }
-	);
-	findOptimalPath(valves, 'AA', closedValves, 30);
+      return { valves, closedValves };
+    },
+    { valves: {}, closedValves: [] }
+  );
 
-	// let minutes = 30;
-	// let releasedPressure = 0;
-	// let currentPosition = 'AA';
-
-	// while (minutes > 0 && Object.keys(toOpen).length) {
-	// 	const nextValve = Object.keys(toOpen)
-	// 		.map((valve) => {
-	// 			const shortestPath = findShortestPathBetweenValves(
-	// 				currentPosition,
-	// 				valve,
-	// 				valves
-	// 			);
-	// 			return {
-	// 				valve,
-	// 				distance: shortestPath,
-	// 				totalPressureReleased: (minutes - shortestPath - 1) * toOpen[valve],
-	// 			};
-	// 		})
-	// 		.sort(
-	// 			(a, b) =>
-	// 				a.totalPressureReleased * valves[a.valve].length -
-	// 				b.totalPressureReleased * valves[b.valve].length
-	// 		)
-	// 		.pop();
-
-	// 	if (nextValve.totalPressureReleased > 0) {
-	// 		currentPosition = nextValve.valve;
-	// 		releasedPressure += nextValve.totalPressureReleased;
-	// 	}
-
-	// 	delete toOpen[nextValve.valve];
-	// 	minutes -= nextValve.distance + 1;
-	// }
-
-	return {
-		1: null,
-		2: null,
-	};
+  return {
+    1: findOptimalPath(valves, 'AA', closedValves, 30),
+    2: null,
+  };
 }
 
 const findShortestPathBetweenValves = (startValve, targetValve, valves) => {
-	const toVisit = Object.fromEntries(
-		Object.entries(valves).map(([valve, connections]) => [
-			valve,
-			{ valve, connections, distance: Infinity, visited: false },
-		])
-	);
+  const toVisit = Object.fromEntries(
+    Object.entries(valves).map(([valve, { connections }]) => [
+      valve,
+      { valve, connections, distance: Infinity, visited: false },
+    ])
+  );
 
-	toVisit[startValve].distance = 0;
+  toVisit[startValve].distance = 0;
 
-	let pq = new PriorityQueue();
-	pq.enqueue(toVisit[startValve], toVisit[startValve].distance);
+  let pq = new PriorityQueue();
+  pq.enqueue(toVisit[startValve], toVisit[startValve].distance);
 
-	while (pq.items.length && !toVisit[targetValve].visited) {
-		const nextValve = pq.dequeue().element;
-		toVisit[nextValve.valve].visited = true;
+  while (pq.items.length && !toVisit[targetValve].visited) {
+    const nextValve = pq.dequeue().element;
+    toVisit[nextValve.valve].visited = true;
 
-		nextValve.connections.forEach((valve) => {
-			const distanceFromNext = nextValve.distance + 1;
-			if (distanceFromNext < toVisit[valve].distance) {
-				toVisit[valve].distance = distanceFromNext;
-				pq.enqueue(toVisit[valve], distanceFromNext);
-			}
-		});
-	}
+    nextValve.connections.forEach((valve) => {
+      const distanceFromNext = nextValve.distance + 1;
+      if (distanceFromNext < toVisit[valve].distance) {
+        toVisit[valve].distance = distanceFromNext;
+        pq.enqueue(toVisit[valve], distanceFromNext);
+      }
+    });
+  }
 
-	return toVisit[targetValve].distance;
+  return toVisit[targetValve].distance;
 };
 
-const findOptimalPath = (valves, currentPosition, closedValves, minutes) => {
-	const paths = closedValves
-		.map((valve) => {
-			const shortestPath = findShortestPathBetweenValves(
-				currentPosition,
-				valve,
-				valves
-			);
-			return {
-				valve,
-				distance: shortestPath,
-				totalPressureReleased: (minutes - shortestPath - 1) * valves[valve].flowRate,
-			};
-		})
+const findOptimalPath = (
+  valves,
+  currentPosition,
+  closedValves,
+  minutes,
+  releasedPressure = 0
+) =>
+  closedValves.reduce((totalPressure, valve) => {
+    const timeToOpen =
+      findShortestPathBetweenValves(currentPosition, valve, valves) + 1;
 
-	console.log(paths)
-}
+    if (timeToOpen > minutes) {
+      return totalPressure;
+    }
+
+    const pressureAtValve =
+      releasedPressure + (minutes - timeToOpen) * valves[valve].flowRate;
+    const pressure = findOptimalPath(
+      valves,
+      valve,
+      closedValves.filter((o) => o !== valve),
+      minutes - timeToOpen,
+      pressureAtValve
+    );
+
+    return pressure > totalPressure ? pressure : totalPressure;
+  }, releasedPressure);
