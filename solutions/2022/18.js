b5d58982d761b0b45a3bb1587a7c1e939f4aeaa4
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-18 19:05:52 +0000

    part 1 solution and verified answer

--- solutions/2022/18.js
+++ solutions/2022/18.js
@@ -1,9 +1,35 @@
+import { sum } from '../../utils/general/array-tools';
+
 export default function (inputFile) {
+  const cubes = inputFile.reduce((cubes, cube) => {
+    const [x, y, z] = cube.match(/\d+/g).map((coordinate) => +coordinate);
 
+    const openSides = getNeighbours(x, y, z).reduce((sides, neighbour) => {
+      if (cubes[neighbour]) {
+        sides--;
+        cubes[neighbour]--;
+      }
 
+      return sides;
+    }, 6);
+    cubes[`${x}|${y}|${z}`] = openSides;
 
-	return {
-		1: null,
-		2: null
-	}
+    return cubes;
+  }, {});
+
+  const surfaceArea = sum(Object.values(cubes));
+
+  return {
+    1: surfaceArea,
+    2: null,
+  };
 }
+
+const getNeighbours = (x, y, z) => [
+  `${x - 1}|${y}|${z}`,
+  `${x + 1}|${y}|${z}`,
+  `${x}|${y - 1}|${z}`,
+  `${x}|${y + 1}|${z}`,
+  `${x}|${y}|${z - 1}`,
+  `${x}|${y}|${z + 1}`,
+];
