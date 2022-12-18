45688365350ef0ad57d3916e99ee88dae1f4b872
Philip Rozycki-Burn <p.rozyckiburn@icloud.com>
2022-12-18 10:50:19 +0000

    part 1 solution

--- solutions/2022/17.js
+++ solutions/2022/17.js
@@ -1,9 +1,20 @@
+import { TetrisCave } from '../../utils/2022/tetris-rocks';
+
 export default function (inputFile) {
+  const cave = new TetrisCave(7, inputFile);
 
+  for (let i = 0; i < 2022; i++) {
+    cave.dropRock();
+  }
 
+  //   const cave2 = new TetrisCave(7, inputFile);
 
-	return {
-		1: null,
-		2: null
-	}
+  //   for (let i = 0; i < 1000000000000; i++) {
+  //     cave2.dropRock();
+  //   }
+
+  return {
+    1: cave.height,
+    2: null,
+  };
 }
---
+++ utils/2022/tetris-rocks.js
@@ -0,0 +1,117 @@
+export class TetrisCave {
+  constructor(width, jetPattern) {
+    this.width = width;
+    this.height = 0;
+
+    this.gasJets = jetPattern[0].split('').map((jet) => (jet === '>' ? 1 : -1));
+
+    this.settledRockPositions = new Set();
+    this.rockShapeIndex = 0;
+
+    this.rock = new TetrisRock(this.width);
+  }
+
+  dropRock() {
+    this.rock.reset(this.rockShapeIndex % 5, this.height + 3);
+    this.rockShapeIndex++;
+
+    let movementIndex = 0;
+    while (!this.rock.isSettled) {
+      if (movementIndex % 2) {
+        this.rock.move(
+          'y',
+          -1,
+          this.width,
+          this.height,
+          this.settledRockPositions
+        );
+      } else {
+        const distance = this.gasJets.shift();
+        this.rock.move(
+          'x',
+          distance,
+          this.width,
+          this.height,
+          this.settledRockPositions
+        );
+        this.gasJets.push(distance);
+      }
+      movementIndex++;
+    }
+    this.addSettledRocks(this.rock.position);
+  }
+
+  addSettledRocks(positions) {
+    positions.forEach((position) => {
+      this.settledRockPositions.add(`${position.x}|${position.y}`);
+      this.height = Math.max(this.height, position.y + 1);
+    });
+  }
+}
+
+class TetrisRock {
+  constructor() {}
+
+  reset(shape, height) {
+    this.position = this.getShape(shape, height);
+    this.isSettled = false;
+  }
+
+  getShape(shape, height) {
+    switch (shape) {
+      case 0:
+        return Array.from({ length: 4 }, (_, i) => ({
+          x: 2 + i,
+          y: height,
+        }));
+
+      case 1:
+        return [
+          {
+            x: 2,
+            y: height + 1,
+          },
+          ...Array.from({ length: 3 }, (_, i) => ({ x: 3, y: height + i })),
+          {
+            x: 4,
+            y: height + 1,
+          },
+        ];
+
+      case 2:
+        return [
+          ...Array.from({ length: 3 }, (_, i) => ({ x: 2 + i, y: height })),
+          ...Array.from({ length: 2 }, (_, i) => ({ x: 4, y: height + i + 1 })),
+        ];
+
+      case 3:
+        return Array.from({ length: 4 }, (_, i) => ({ x: 2, y: height + i }));
+
+      case 4:
+        return [
+          ...Array.from({ length: 2 }, (_, i) => ({ x: 2 + i, y: height })),
+          ...Array.from({ length: 2 }, (_, i) => ({ x: 2 + i, y: height + 1 })),
+        ];
+    }
+  }
+
+  move(axis, distance, width, height, settledRockPositions) {
+    const newPosition = this.position.map((part) => ({
+      ...part,
+      [axis]: part[axis] + distance,
+    }));
+    if (
+      newPosition.every(
+        (part) =>
+          part.x >= 0 &&
+          part.x < width &&
+          part.y >= 0 &&
+          !settledRockPositions.has(`${part.x}|${part.y}`)
+      )
+    ) {
+      this.position = newPosition;
+    } else if (axis === 'y') {
+      this.isSettled = true;
+    }
+  }
+}
